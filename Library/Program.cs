d07836b5aef10964a01dca8ec434909c6635ea1f
Patryk Kozarski <Patryknerio2@gmail.com>
2019-10-30 16:04:32 +0100

    lf top 10 autors by name + his books

--- Library/Program.cs
+++ Library/Program.cs
@@ -7,6 +7,43 @@ using System.Threading.Tasks;
 namespace Library {
 	class Program {
 		static void Main(string [] args) {
+			Console.WriteLine("dziala");
+			LibraryDataContext ldb = new LibraryDataContext();
+			
+			char c;
+			string name="";
+			do {
+				Console.Clear();
+				Console.WriteLine("Press \"`\" to Exit");
+				Console.WriteLine("Looking for: " + name);
+				Console.WriteLine("");
+				var data = findAuthors(ldb , name , "");
+				foreach ( var author in data ) {
+					Console.WriteLine("====================");
+					Console.WriteLine(author.name + " , " + author.authorId);
+					Console.WriteLine("Books:");
+					var bookData = BooksOfAuthor(ldb , author.authorId);
+					foreach ( var book in bookData ) {
+						Console.WriteLine(book.name);
+					}
+				}
+				c = Console.ReadKey().KeyChar;
+				name += c;
+			} while ( c != '`' );
+
+		}
+		public static books [] BooksOfAuthor(LibraryDataContext ldb , int authorId) {
+			var data = ldb.books.Where(b => b.authorId == authorId).ToArray();
+			return data;
+		}
+
+		public static authors [] findAuthors(LibraryDataContext ldb , string firstName , string secondName) {
+			authors [] aut = ldb.authors
+				.Where(a => a.name.Contains(firstName))
+				.Where(a => a.surname.Contains(secondName))
+				.Take(10).ToArray();
+			return aut;
 		}
 	}
+
 }
